e38d9d8d06ac32292443001cf9e632c39c844ecb
Sunil Kumar <52163869+suniljalndhra@users.noreply.github.com>
2023-04-27 08:05:22 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,133 @@
+jQuery("#carousel").owlCarousel({
+  autoplay: true,
+  rewind: true, /* use rewind if you don't want loop */
+  margin: 30,
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  dots:false,
+  loop: true,
+  nav: true,
+  navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 2,
+      margin: 15
+    },
+
+    1024: {
+      items: 3
+    },
+
+    1366: {
+      items: 3
+    }
+  }
+});
+jQuery(".opinion").owlCarousel({
+  autoplay: true,
+  rewind: true, /* use rewind if you don't want loop */
+  margin: 30,
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  dots:false,
+  loop: true,
+  nav: true,
+  navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 2,
+      margin: 15
+    },
+
+    1024: {
+      items: 2
+    },
+
+    1366: {
+      items: 3
+    }
+  }
+});
+
+jQuery(".upcoming_carousel").owlCarousel({
+  autoplay: true,
+  rewind: true, /* use rewind if you don't want loop */
+  margin: 0,
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  dots:false,
+  loop: true,
+  nav: true,
+  navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 1
+    },
+
+    1024: {
+      items: 2
+    },
+
+    1366: {
+      items: 2
+    }
+  }
+});
+
+jQuery(".carousel_reports").owlCarousel({
+  autoplay: true,
+  rewind: true, /* use rewind if you don't want loop */
+  margin: 40,
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  dots:false,
+  loop: true,
+  nav: true,
+  navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 1.5,
+      margin: 20
+    },
+
+    1024: {
+      items: 1.5
+    },
+
+    1366: {
+      items:2.5
+    }
+  }
+});
+
+jQuery(".burget_menu").click(function(){
+    jQuery(".wrapper_navigation").addClass('add_menu');
+})
+
+jQuery(".crss_buttons").click(function(){
+    jQuery(".wrapper_navigation").removeClass('add_menu');
+})
